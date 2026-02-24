54a0fc7c27208dad619735556dad1fba3021b9a7
Kely <citizenkds@gmail.com>
2026-02-24 17:29:51 -0300

    Add exercise selection feature in quartalista.py
    Implement exercise selection feature with multiple options for user input and calculations.

---
+++ quartalista.py
@@ -0,0 +1,71 @@
+indice = int(input("Qual o exercício desejado? (1 a 8): "))
+
+if indice == 1:
+  n = int(input())
+
+  for i in range(n):
+    print (i)
+
+elif indice == 2:
+  n = int(input())
+
+  for i in range(n):
+    num = i + 1
+    print (num)
+
+elif indice == 3:
+  n = int(input())
+
+  for i in range(1, 12):
+    tabuada = n * i
+    print(tabuada)
+
+elif indice == 4:
+  ntabuada = int(input())
+  iniciotabuada = int(input())
+  fimtabuada = int(input())
+
+  for i in range(iniciotabuada, fimtabuada + 1):
+    tabuada = ntabuada * i
+    print(ntabuada, "x", i, "=", tabuada)
+  
+elif indice == 5:
+  coisa = input()
+  qtdeenrol = int(input())
+
+  for i in range (1, qtdeenrol + 1):
+    print("enrolando", coisa, i)
+  
+elif indice == 6:
+  n = int(input())
+  s = ''
+
+  for i in range(n):
+    s = ''
+    for j in range(n):
+      if j == i or j + i == n - 1:
+        s = s + '+ '
+      else:
+        s = s + '* '
+    print(s)
+
+elif indice == 7:
+  nmoedas = int(input())
+  total = 0
+
+  for i in range(nmoedas):
+    valor = float(input())
+    total = total + valor
+  print('%.2f' % total)
+  
+elif indice == 8:
+  napostas = int(input())
+  total = 0
+
+  for i in range(napostas):
+    valor = float(input())
+    total = total + valor
+  print('%.2f' % total)
+
+else:
+  print("Command not found")
